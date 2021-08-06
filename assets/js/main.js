4273acb58733bd359616af1f0a51a23c41cd4170
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-06 19:59:37 +0700

    add function to close menu list each click any menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ if(navClose) {
         navMenu.classList.remove('show-menu');
     });
 }
+
+/* Remove menu mobile */
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When click on each nav__link, remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
