ef2d58bd67e17aae89ec365556bdf3efc98dae20
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-08-11 14:19:59 +0700

    add function for interacting with services modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,93 @@
 /* Menu Show / Hidden */
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /* Menu Show */
 // validate if navToggle exist
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /* Menu Hidden */
 // validate if navToggle exist
-if(navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /* Remove menu mobile */
 
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    // When click on each nav__link, remove the show-menu class
-    navMenu.classList.remove('show-menu');
+  const navMenu = document.getElementById("nav-menu");
+  // When click on each nav__link, remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* Accordion Skills */
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader  = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className;
+  let itemClass = this.parentNode.className;
 
-    for(i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if(itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open'
-    }
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
+  el.addEventListener("click", toggleSkills);
 });
 
 /* Qualification Tabs */
-const tabs          = document.querySelectorAll('[data-target'),
-      tabContens    = document.querySelectorAll('[data-content');
+const tabs = document.querySelectorAll("[data-target"),
+  tabContens = document.querySelectorAll("[data-content");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContens.forEach(tabContent => {
-            tabContent.classList.remove('qualification__active');
-        });
-        target.classList.add('qualification__active');
-
-        tabs.forEach(tab => {
-            tab.classList.remove('qualification__active');
-        });
-        tab.classList.add('qualification__active');
+    tabContens.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
     });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+/* Services Modal */
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalCLose) => {
+  modalCLose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
 });
